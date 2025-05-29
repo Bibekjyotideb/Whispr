00ade8a63564bc16be8c8a7f0f142811975e3c0f
Bibekjyotideb <95222438+Bibekjyotideb@users.noreply.github.com>
2025-05-29 07:16:23 +0530

    Update message.route.js

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -5,8 +5,9 @@ import { sendMessage, getMessages, getUsersForSidebar } from "../controllers/mes
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.post("/send/:id", protectRoute, sendMessage);
 router.get("/:id", protectRoute, getMessages);
 
-router.post("/send/:id", protectRoute, sendMessage);
+
 
 export default router;
